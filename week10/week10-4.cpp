26fd4fc181ed178bccb11be537c071caeac2868e
kai817798 <hyk20060817@gmail.com>
2025-04-25 12:06:22 +0800

    week10

---
+++ week10/week10-4.cpp
@@ -0,0 +1,24 @@
+//week10-4.cpp
+//LeetCode 1399.Count Largest Group
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        int a[100]={};
+        int largest = 0;
+        for(int i=1;i<=n;i++){//1..n¤HÃþªº°j°é
+            int now = i;
+            int total = 0;
+            while(now>0){
+                total += now % 10;
+                now = now/10;
+            }
+            a[total]++;
+            if(a[total]>largest)largest = a[total];
+        }
+        int ans = 0;
+        for(int i=0;i<100;i++){
+            if(a[i]==largest)ans++;
+        }
+        return ans;
+    }
+};
